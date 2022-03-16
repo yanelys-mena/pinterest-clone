48701c6c5a36aa3edf0873dcabb914b2cb4e92cc
yanelys-mena <yanelyshm@outlook.com>
2022-03-16 19:14:10 -0400

    buttons positioned on card

--- react-app/src/components/PinGrid/index.js
+++ react-app/src/components/PinGrid/index.js
@@ -1,14 +1,22 @@
 import PinCard from './PinCard';
+import InfiniteScroll from 'react-infinite-scroll-component';
 import './PinGrid.css';
 
 export default function PinGrid({ pins }) {
     console.log('fromgrid', pins)
     return (
-        <div id="pinGrid">
-            {pins.map(pin => (
-                <PinCard pin={pin} key={pin.id} />
-            ))}
+        <InfiniteScroll
+            dataLength={pins.length}
+            loader={<>Load More</>}
+            endMessage={<>no more pins</>}>
+            <div id="pinGrid">
 
-        </div>
+                {pins.map(pin => (
+                    <PinCard pin={pin} key={pin.id} />
+                ))}
+
+
+            </div >
+        </InfiniteScroll>
     )
 }
