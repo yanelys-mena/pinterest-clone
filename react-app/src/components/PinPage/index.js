d2c5e840b8bec78c9732974f23ea89ff4ded334f
yanelys-mena <yanelyshm@outlook.com>
2022-03-16 23:45:17 -0400

    pin page 90% complete

--- react-app/src/components/PinPage/index.js
+++ react-app/src/components/PinPage/index.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { useSelector } from 'react-redux'
 import { useParams, useHistory, Link } from 'react-router-dom'
 import './PinPage.css'
@@ -9,10 +10,13 @@ export default function PinPage() {
     const pin = useSelector(state => state?.pins[pinId])
     const user = useSelector(state => state?.session?.user)
 
+    const [showComments, setShowComments] = useState(true)
+
     console.log(pin)
 
     return (
         <div id="pinPage">
+
             <div id='pinPageArrow' >
                 <div onClick={history.goBack} id="arrowClick"><i className="fa-solid fa-arrow-left"></i></div>
             </div>
@@ -25,17 +29,54 @@ export default function PinPage() {
                         <button id="pinSaveBtn">Save</button>
                     </div>
                     <div id="pinPageInfo">
-                        <div id="link"><Link to={pin?.link} target="_blank">{pin?.link.substring(0, 50)}... </Link></div>
+                        <div id="link">
+                            {pin.link ? <Link to={pin?.link} target="_blank">{pin?.link.substring(0, 50)}... </Link> : ''}
+
+                        </div>
                         <div>{pin.title}</div>
-                        <div id="description">{pin?.description}</div>
+                        {pin?.description ?
+                            <div id="description">{pin.description} </div> : ''}
                         <div id="userInfo">
                             <div id="userPhoto"><img src={user?.photo} alt='userPhoto'></img></div>
                             <div>{user?.username}</div>
-                            <div>followbutton</div>
+                            {/* <div>followbutton</div> */}
                         </div>
                     </div>
                     <div id="comments">
-                        <div>{pin?.comments.map(comment => <div>{comment?.content}</div>)}</div>
+                        {!showComments && <div id="commentsHeader" >
+                            <div>Comments</div>
+                            <div onClick={() => setShowComments(true)}> <i class="fa-solid fa-chevron-right"></i></div>
+                        </div>
+
+                        }
+                        {showComments &&
+                            <>
+                                <div id="commentsHeader" >
+                                    <div>Comments</div>
+                                    <div onClick={() => setShowComments(false)}> <i className="fa-solid fa-chevron-down"></i></div>
+                                </div>
+                                <div id="allComments">{pin?.comments.map(comment => <div id="indComment">{comment?.content}</div>)}</div>
+
+                                <div id="leaveComment">
+                                    <div id="commentTip">Share feedback, ask a question or give a high five
+                                    </div>
+                                    <div id="commentInputDiv">
+                                        <div id="userPhoto_comment"><img src={user?.photo} alt='userPhoto'></img></div>
+                                        <div id="formDiv">
+                                            <form disabled="disabled">
+                                                <input
+                                                    id="commentInput"
+                                                    type='text'
+                                                    placeholder='Comments coming soon'
+                                                ></input>
+                                            </form>
+                                        </div>
+                                    </div>
+
+                                </div>
+                            </>
+                        }
+
                     </div>
                 </div>
             </div>
