3528893a4f6c5a1953f0517c5b3e37e6d1a0f614
yanelys-mena <yanelyshm@outlook.com>
2022-03-17 18:43:49 -0400

    edit button position

--- react-app/src/components/BoardGrid/BoardCard.js
+++ react-app/src/components/BoardGrid/BoardCard.js
@@ -8,13 +8,17 @@ export default function BoardCard({ board }) {
 
     return (
         <div id="boardCard">
+
+
             <Link to={`/boards/${board?.id}`} style={{ textDecoration: 'none' }}>
                 <div id="boardContainer">
 
                     {/* <div> */}
                     {board.pins.length > 0 ? <img src={board.pins[0].image} alt="board" id="boardimg"></img> : <img src="https://wallpaperaccess.com/full/1091550.jpg" alt="no_pins_in_board"></img>}
                     {/* </div> */}
-
+                    <div>
+                        <i className="fa-solid fa-pen" id="editBoardBtn"></i>
+                    </div>
                 </div>
                 <div id="boardName">{board.name}</div>
             </Link>
