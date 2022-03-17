ad6424ba338d99b16cbf7fddb2e506c25810c4e1
yanelys-mena <yanelyshm@outlook.com>
2022-03-17 18:59:18 -0400

    edit button linked with absolute position icon

--- react-app/src/components/BoardGrid/BoardCard.js
+++ react-app/src/components/BoardGrid/BoardCard.js
@@ -1,27 +1,50 @@
 import './BoardCard.css'
 import { Link } from 'react-router-dom'
+import { useState } from 'react'
+import { useSelector } from 'react-redux'
+import { Modal } from '../../context/Modal'
+import CreateBoardForm from '../CreateBoardModal/CreateBoardForm'
 
 export default function BoardCard({ board }) {
-
-
+    const [style, setStyle] = useState({ display: 'none' })
+    const [showModal, setShowModal] = useState(false)
+    const user = useSelector(state => state?.session?.user)
 
 
     return (
         <div id="boardCard">
 
 
-            <Link to={`/boards/${board?.id}`} style={{ textDecoration: 'none' }}>
-                <div id="boardContainer">
 
-                    {/* <div> */}
+            <div
+                id="boardContainer"
+                onMouseEnter={e => {
+                    setStyle({ display: 'inline' });
+                }}
+                onMouseLeave={e => {
+                    setStyle({ display: 'none' });
+                }}>
+                <Link to={`/boards/${board?.id}`} style={{ textDecoration: 'none' }}>
                     {board.pins.length > 0 ? <img src={board.pins[0].image} alt="board" id="boardimg"></img> : <img src="https://wallpaperaccess.com/full/1091550.jpg" alt="no_pins_in_board"></img>}
-                    {/* </div> */}
-                    <div>
-                        <i className="fa-solid fa-pen" id="editBoardBtn"></i>
-                    </div>
+                </Link>
+                {/* <div> */}
+                <div style={style} onClick={() => setShowModal(true)}>
+                    <i className="fa-solid fa-pen" id="editBoardBtn"  ></i>
                 </div>
-                <div id="boardName">{board.name}</div>
-            </Link>
+
+
+            </div>
+
+            <div id="boardName">{board.name}</div>
+
+
+            {
+                showModal && (
+                    <Modal onClose={() => setShowModal(false)}>
+                        <CreateBoardForm user={user} setShowModal={setShowModal} board={board} />
+                    </Modal>
+                )
+            }
         </div>
     )
 }
