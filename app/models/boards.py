b537ea5ecf025a5574f9ed2dd06b39b8bd50cc16
yanelys-mena <yanelyshm@outlook.com>
2022-03-19 18:45:31 -0400

    changed board unique

--- app/models/boards.py
+++ app/models/boards.py
@@ -7,7 +7,7 @@ class Board(db.Model):
     __tablename__ = 'boards'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), nullable=False, unique=True)
+    name = db.Column(db.String(50), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
 
     user = db.relationship("User", back_populates='boards')
