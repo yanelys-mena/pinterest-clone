59676e07851193018c775739bad2ee37bcc0f045
yanelys-mena <yanelyshm@outlook.com>
2022-03-19 00:22:29 -0400

    setting up login form structure

--- react-app/src/components/LandingPage/LandingPageGrid.js
+++ react-app/src/components/LandingPage/LandingPageGrid.js
@@ -1,11 +1,18 @@
+import { useEffect, useState } from "react";
+
+
 export default function LandingPageGrid() {
     return (
-        <div id="pinGrid">
-            {Array(8).map((i, idx) => (
-                <PinCard key={idx} />
-            ))}
+        <div id="landingGrid">
+            {[...Array(8)].map((i, idx) =>
+                <>
 
+                    <div id="fadeIn">
+                        <img style={{ height: '100px' }} src="https://www.creativelive.com/blog/wp-content/uploads/2018/08/Stocksy_txp3af6e914r0u000_Medium_896968-1.jpg"></img>
+                    </div>
+                </>
+            )}
 
-        </div >
+        </div>
     )
 }
--- react-app/src/components/LandingPage/index.js
+++ react-app/src/components/LandingPage/index.js
@@ -1,5 +1,7 @@
 import './LandingPage.css';
 import { useRef, useEffect } from 'react';
+import LandingPageGrid from './LandingPageGrid';
+import LoginForm from '../auth/LoginForm'
 
 export default function LandingPage() {
 
@@ -20,22 +22,15 @@ export default function LandingPage() {
             landing page
 
             <link href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.css" rel="stylesheet" />
-            <ul>
-                <li><img src="https://www.creativelive.com/blog/wp-content/uploads/2018/08/Stocksy_txp3af6e914r0u000_Medium_896968-1.jpg" /></li>
-                <li><img src="https://www.creativelive.com/blog/wp-content/uploads/2018/08/Stocksy_txp3af6e914r0u000_Medium_896968-1.jpg" /></li>
-                <li><img src="https://www.creativelive.com/blog/wp-content/uploads/2018/08/Stocksy_txp3af6e914r0u000_Medium_896968-1.jpg" /></li>
-                <li><img src="https://www.creativelive.com/blog/wp-content/uploads/2018/08/Stocksy_txp3af6e914r0u000_Medium_896968-1.jpg" /></li>
-                <li><img src="https://www.creativelive.com/blog/wp-content/uploads/2018/08/Stocksy_txp3af6e914r0u000_Medium_896968-1.jpg" /></li>
-                <li><img src="https://www.creativelive.com/blog/wp-content/uploads/2018/08/Stocksy_txp3af6e914r0u000_Medium_896968-1.jpg" /></li>
-                <li><img src="https://www.creativelive.com/blog/wp-content/uploads/2018/08/Stocksy_txp3af6e914r0u000_Medium_896968-1.jpg" /></li>
-                <li><img src="https://www.creativelive.com/blog/wp-content/uploads/2018/08/Stocksy_txp3af6e914r0u000_Medium_896968-1.jpg" /></li>
-            </ul>
-            {/* <div id='fadeIn'>
-                <img src='https://www.creativelive.com/blog/wp-content/uploads/2018/08/Stocksy_txp3af6e914r0u000_Medium_896968-1.jpg'></img>
-            </div> */}
-            <button onClick={scrollToBottom} id="landing_arrow"><i className="fa-solid fa-angle-down "></i></button>
+            <div id="topPage">   <LandingPageGrid /></div>
+            <div id="bottomPage">
+                <div>HEYPPP</div>
+                <LoginForm />
+            </div>
+
+            <button onClick={scrollToBottom} id="landing_arrow" > <i className="fa-solid fa-angle-down "></i></button >
             <div ref={messagesEndRef} />
-        </div>
+        </div >
     )
 }
 
