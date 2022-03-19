b5f25a44ce8f25e82e25765172b406dc0c5f305e
yanelys-mena <yanelyshm@outlook.com>
2022-03-19 19:02:25 -0400

    navbar fixed

--- react-app/src/components/NavBar/index.js
+++ react-app/src/components/NavBar/index.js
@@ -25,44 +25,48 @@ const NavBar = () => {
             <NavLink to='/' exact={true} activeClassName='active'><button id="homeButton">Home</button></NavLink>
           </div>
         </div>
-        <div id="searchNav">
-          <SearchBar />
-        </div>
 
         {user &&
-          <div id="rightNav">
-            <div className="faIcons">
+          <div div id="searchNav">
+            <SearchBar />
+          </div>}
 
+        <div id="rightNav">
+          {user &&
+            <div className="faIcons">
               <NavLink to="#"><i className="fa-solid fa-bell"> </i></NavLink>
-            </div>
+            </div>}
+          {user &&
             <div className="faIcons">
               <NavLink to="/pinbuilder"> <i className="fa-solid fa-circle-plus"></i>
               </NavLink>
-            </div>
-            <div className="faIcons">
-              <NavLink to={`/profile/${user?.id}`}>
-                {user?.photo ? <img id="navUserPhoto" src={user.photo} alt='userphoto'></img> : <i className="fas fa-user-circle"></i>}</NavLink>
-
-            </div>
+            </div>}
+          {user && <div className="faIcons">
+            <NavLink to={`/profile/${user?.id}`}>
+              {user?.photo ? <img id="navUserPhoto" src={user.photo} alt='userphoto'></img> : <i className="fas fa-user-circle"></i>}</NavLink>
+          </div>}
+          {user &&
             <div id="accoutIcon" >
               {user && <AccountMenu user={user} />}
             </div>
-          </div>
-        }
-        {!user &&
-          <>
-            <div id="login_nav_button" onClick={() => setShowModal(true)}>
-              <NavLink to='/login' exact={true} activeClassName='active'>
-                Login
-              </NavLink>
-            </div>
-            <div id="signup_nav_button" onClick={() => setShowModal(true)}>
-              <NavLink to='/sign-up' exact={true} activeClassName='active'>
-                Sign Up
-              </NavLink>
-            </div>
-          </>
-        }
+          }
+          {!user &&
+            <>
+              <div id="login_nav_button" onClick={() => setShowModal(true)}>
+                <NavLink to='/login' exact={true} activeClassName='active'>
+                  Login
+                </NavLink>
+              </div>
+              <div id="signup_nav_button" onClick={() => setShowModal(true)}>
+                <NavLink to='/sign-up' exact={true} activeClassName='active'>
+                  Sign Up
+                </NavLink>
+              </div>
+            </>
+          }
+        </div>
+
+
         {showModal && (
           <Modal onClose={() => setShowModal(false)}>
             <LoginModal />
