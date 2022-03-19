0661bafae76eb5e170ecd1180284ccf788ad8479
yanelys-mena <yanelyshm@outlook.com>
2022-03-18 21:19:37 -0400

    board remove buttons css done

--- react-app/src/components/UnpinModal/RemovePin.js
+++ react-app/src/components/UnpinModal/RemovePin.js
@@ -1,7 +1,19 @@
+import { useSelector } from "react-redux";
+
+
 export default function RemovePin() {
+    const boards = useSelector(state => Object.values(state?.boards))
+
     return (
         <div id="removePin">
-            remove pins
-        </div>
+            <div id="removePin_boards">
+                <div class="removePin_boardbtn">
+                    <div class="close"></div>
+                    <div class="message">
+                        Action Successful!
+                    </div>
+                </div>
+            </div>
+        </div >
     )
 }
