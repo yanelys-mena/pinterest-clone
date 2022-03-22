02cc9424a03cacb1c97a5ea792bb0224b42b881a
yanelys-mena <yanelyshm@outlook.com>
2022-03-22 19:26:17 -0400

    Sign up error length with username is fixed

--- app/forms/signup_form.py
+++ app/forms/signup_form.py
@@ -35,5 +35,5 @@ def confirm_password_matches(form, field):
 class SignUpForm(FlaskForm):
     username = StringField('username', validators=[DataRequired('Please enter a username.'), username_exists, Length(min=3,max=30,message='Username must be between 3 and 30 characters')])
     email = StringField('email', validators=[DataRequired('Please enter an email.'), user_exists, Length(min=5, max=255, message='Please enter an email between 5 and 30 characters.'), Email(message='Please enter a valid email address.')])
-    password = StringField('password', validators=[DataRequired('Please enter a password.'), EqualTo('confirm_password', message='Passwords must match.'), Length(min=7,max=100,message='Username must be between 3 and 30 characters')])
-    confirm_password = StringField('confirm_password', validators=[DataRequired(message='Please confirm your password.'), Length(min=7,max=100,message='Username must be between 3 and 30 characters')])
+    password = StringField('password', validators=[DataRequired('Please enter a password.'), EqualTo('confirm_password', message='Passwords must match.'), Length(min=7,max=100,message='Password must be between 8 and 100 characters')])
+    confirm_password = StringField('confirm_password', validators=[DataRequired(message='Please confirm your password.'), Length(min=7,max=100,message='Password must be between 8 and 100 characters')])
