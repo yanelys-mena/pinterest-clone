fca861c3797041386237a477a6fb7c44de179fbf
yanelys-mena <yanelyshm@outlook.com>
2022-03-23 11:36:01 -0400

    changes to css

--- react-app/src/components/BoardGrid/index.js
+++ react-app/src/components/BoardGrid/index.js
@@ -8,7 +8,7 @@ export default function BoardGrid({ boards, profileId }) {
 
     return (
         <div id="board_page_grid">
-            {boards?.length ? <>{boards.map(board => <BoardCard profileId={profileId} board={board} key={board?.id} />)}</> : 'No boards added. Create a new board to start adding pins!'}
+            {boards?.length ? <>{boards.map(board => <BoardCard profileId={profileId} board={board} key={board?.id} />)}</> : 'No boards added yet!'}
         </div>
     )
 }
