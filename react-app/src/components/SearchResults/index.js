a97eb8c789a63d5d166000a256b9ded9e93b925d
yanelys-mena <yanelyshm@outlook.com>
2022-04-13 13:31:52 -0400

    cleaning up code and console

--- react-app/src/components/SearchResults/index.js
+++ react-app/src/components/SearchResults/index.js
@@ -1,7 +1,18 @@
 import './SearchResults.css'
+import { load_pins } from '../../store/pins';
+import { useDispatch, useSelector, } from 'react-redux';
 
 export default function SearchResults() {
     const { searchInput } = useParams();
+    const user = useSelector((state) => state.session?.user);
+    const pins = useSelector(state => Object.values(state?.pins));
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(load_pins())
+    }, [dispatch]);
+
+
     return (
         <>
             search results page
